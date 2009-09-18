f976822e9431bde7643d7c6e76d609281952eb53
Mathieu Baudier <mbaudier@argeo.org>
2009-09-18 15:26:52 +0000

    Introduce Server OSGi
    git-svn-id: https://svn.argeo.org/commons/trunk@2968 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ server/runtime/org.argeo.server.osgi/src/main/java/org/argeo/server/osgi/mvc/ListBundlesController.java
@@ -0,0 +1,26 @@
+package org.argeo.server.osgi.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.BundleContext;
+import org.springframework.osgi.context.BundleContextAware;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class ListBundlesController extends AbstractController implements
+		BundleContextAware {
+	private BundleContext bundleContext;
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+}
