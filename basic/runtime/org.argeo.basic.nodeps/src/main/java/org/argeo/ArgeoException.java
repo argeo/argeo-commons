3726464fdf85bf773fa1cd4a3510f90d566808bf
Mathieu Baudier <mbaudier@argeo.org>
2009-11-25 12:23:53 +0000

    Add Basic No Deps
    git-svn-id: https://svn.argeo.org/commons/trunk@3159 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ basic/runtime/org.argeo.basic.nodeps/src/main/java/org/argeo/ArgeoException.java
@@ -0,0 +1,21 @@
+package org.argeo;
+
+public class ArgeoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ArgeoException() {
+	}
+
+	public ArgeoException(String message) {
+		super(message);
+	}
+
+	public ArgeoException(Throwable cause) {
+		super(cause);
+	}
+
+	public ArgeoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
