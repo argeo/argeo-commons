a9420b9215e813697937c6fcc501e1034d1ad015
Mathieu Baudier <mbaudier@argeo.org>
2011-01-17 14:19:16 +0000

    Small improvements
    git-svn-id: https://svn.argeo.org/commons/trunk@4052 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- security/eclipse/plugins/org.argeo.security.ui.application/src/main/java/org/argeo/security/ui/application/SecureWorkbenchAdvisor.java
+++ security/eclipse/plugins/org.argeo.security.ui.application/src/main/java/org/argeo/security/ui/application/SecureWorkbenchAdvisor.java
@@ -6,13 +6,18 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class SecureWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	static final String PERSPECTIVE_ID = "org.argeo.security.ui.securityPerspective"; //$NON-NLS-1$
+	static final String DEFAULT_PERSPECTIVE_ID = "org.argeo.security.ui.securityPerspective"; //$NON-NLS-1$
 
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new SecureWorkbenchWindowAdvisor(configurer);
-    }
+	public final static String INITIAL_PERSPECTIVE_PROPERTY = "org.argeo.security.ui.initialPerspective";
+	private String initialPerspective = System.getProperty(
+			INITIAL_PERSPECTIVE_PROPERTY, DEFAULT_PERSPECTIVE_ID);
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new SecureWorkbenchWindowAdvisor(configurer);
+	}
 
 	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
+		return initialPerspective;
 	}
 }
