e1e76de107e054371fab6b517dcc5be621ebffc0
Mathieu Baudier <mbaudier@argeo.org>
2011-02-25 19:19:52 +0000

    Extend Spring DM web extender in order to fix issue when undeploying
    git-svn-id: https://svn.argeo.org/commons/trunk@4209 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- server/runtime/org.argeo.server.catalina/src/main/java/org/argeo/server/catalina/TomcatDeployer.java
+++ server/runtime/org.argeo.server.catalina/src/main/java/org/argeo/server/catalina/TomcatDeployer.java
@@ -1,5 +1,26 @@
 package org.argeo.server.catalina;
 
-public class TomcatDeployer {
+import org.apache.catalina.Service;
+import org.springframework.osgi.web.deployer.tomcat.TomcatWarDeployer;
 
+/**
+ * Wraps the Spring DM Tomcate deployer in order to avoid issue with call to
+ * getServerInfo() when undeployed.
+ */
+public class TomcatDeployer extends TomcatWarDeployer {
+	private String serverInfo;
+
+	@Override
+	public void setService(Object service) {
+		super.setService(service);
+
+		serverInfo = ((Service) service).getInfo();
+	}
+
+	@Override
+	protected String getServerInfo() {
+		return serverInfo;
+	}
+
+	
 }
