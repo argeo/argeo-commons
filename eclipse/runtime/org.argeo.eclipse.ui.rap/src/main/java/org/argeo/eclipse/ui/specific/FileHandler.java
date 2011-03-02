f93298706f9e5fd657e260ae0f42cebf74d21a34
Bruno Sinou <bsinou@argeo.org>
2011-03-02 14:29:06 +0000

    add file support for RAP.
    git-svn-id: https://svn.argeo.org/commons/trunk@4269 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- eclipse/runtime/org.argeo.eclipse.ui.rap/src/main/java/org/argeo/eclipse/ui/specific/FileHandler.java
+++ eclipse/runtime/org.argeo.eclipse.ui.rap/src/main/java/org/argeo/eclipse/ui/specific/FileHandler.java
@@ -1,75 +1,22 @@
 package org.argeo.eclipse.ui.specific;
 
-import java.awt.Desktop;
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
-import org.apache.commons.io.IOUtils;
-import org.argeo.ArgeoException;
-
-//import org.apache.commons.io;
+//import org.argeo.eclipse.ui.dialogs.Error;
 
 public class FileHandler {
-
-	private BufferedInputStream bis;
 
 	public FileHandler() {
 	}
 
-	public File createTmpFile(String fileName, String suffix, InputStream is) {
+	public void openFile(String fileName, InputStream is) {
+		// org.argeo.eclipse.ui.dialogs.Error.show("In Open File for RAP.");
+	}
+
+	private File createTmpFile(String prefix, String suffix, InputStream is) {
 		File tmpFile = null;
-		OutputStream os = null;
-		try {
-			tmpFile = File.createTempFile(fileName, suffix);
-			os = new FileOutputStream(tmpFile);
-			IOUtils.copy(is, os);
-		} catch (IOException e) {
-			throw new ArgeoException("Cannot open file " + fileName, e);
-		} finally {
-			IOUtils.closeQuietly(os);
-		}
 		return tmpFile;
 	}
 
-	public void openFile(String fileName, InputStream is) {
-	
-		String prefix ="", extension = "";
-		
-		if (fileName != null){
-			int ind = fileName.
-			if (true){
-				}
-			}
-			
-		prefix = .substring(0,
-				node.getName().lastIndexOf('.'));
-		extension = node.getName().substring(
-				node.getName().lastIndexOf('.'));
-		try {
-			Desktop desktop = null;
-			if (Desktop.isDesktopSupported()) {
-				desktop = Desktop.getDesktop();
-			}
-			desktop.open(file);
-		} catch (IOException e) {
-			throw new ArgeoException("Cannot open file " + file.getName(), e);
-		}
-	}
-	
-	
-	public void openFile(File file) {
-		try {
-			Desktop desktop = null;
-			if (Desktop.isDesktopSupported()) {
-				desktop = Desktop.getDesktop();
-			}
-			desktop.open(file);
-		} catch (IOException e) {
-			throw new ArgeoException("Cannot open file " + file.getName(), e);
-		}
-	}
 }
