239b41dca64d630abc357e67f9d00391008264c9
Mathieu Baudier <mbaudier@argeo.org>
2011-03-24 00:34:24 +0000

    Allows usernames with uppercase
    git-svn-id: https://svn.argeo.org/commons/trunk@4357 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- security/runtime/org.argeo.security.core/src/main/java/org/argeo/security/UserAdminService.java
+++ security/runtime/org.argeo.security.core/src/main/java/org/argeo/security/UserAdminService.java
@@ -4,14 +4,14 @@ import java.util.Set;
 
 import org.springframework.security.userdetails.UserDetailsManager;
 
-public interface UserAdminService extends UserDetailsManager{
+public interface UserAdminService extends UserDetailsManager {
 	/**
 	 * Usernames must match this regexp pattern ({@value #USERNAME_PATTERN}).
 	 * Thanks to <a href=
 	 * "http://www.mkyong.com/regular-expressions/how-to-validate-username-with-regular-expression/"
-	 * >this tip</a> (modified to remove '-')
+	 * >this tip</a> (modified to remove '-' and add upper-case)
 	 */
-	public final static String USERNAME_PATTERN = "^[a-z0-9_]{3,15}$";
+	public final static String USERNAME_PATTERN = "^[a-zA-Z0-9_]{3,15}$";
 
 	/**
 	 * Email addresses must match this regexp pattern ({@value #EMAIL_PATTERN}.
