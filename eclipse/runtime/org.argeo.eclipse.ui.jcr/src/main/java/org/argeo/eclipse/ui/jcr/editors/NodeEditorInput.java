ea64f6c0b88593bb03774e8cde78b278bc9e3948
Mathieu Baudier <mbaudier@argeo.org>
2011-05-02 14:38:16 +0000

    Improve comment
    git-svn-id: https://svn.argeo.org/commons/trunk@4489 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- eclipse/runtime/org.argeo.eclipse.ui.jcr/src/main/java/org/argeo/eclipse/ui/jcr/editors/NodeEditorInput.java
+++ eclipse/runtime/org.argeo.eclipse.ui.jcr/src/main/java/org/argeo/eclipse/ui/jcr/editors/NodeEditorInput.java
@@ -4,6 +4,7 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IPersistableElement;
 
+/** A canonical editor input based on a path to a node. */
 public class NodeEditorInput implements IEditorInput {
 	private final String path;
 
