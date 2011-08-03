1dd21da9a4cb9efe6314b16a6932b32cd2193f63
Mathieu Baudier <mbaudier@argeo.org>
2011-08-03 15:53:21 +0000

    Move Commons GIS to Connect
    git-svn-id: https://svn.argeo.org/commons/trunk@4680 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- gis/plugins/org.argeo.gis.ui/src/main/java/org/argeo/gis/ui/MapViewerListener.java
+++
@@ -1,7 +0,0 @@
-package org.argeo.gis.ui;
-
-public interface MapViewerListener {
-	public void featureSelected(String layerId, String featureId);
-
-	public void featureUnselected(String layerId, String featureId);
-}
