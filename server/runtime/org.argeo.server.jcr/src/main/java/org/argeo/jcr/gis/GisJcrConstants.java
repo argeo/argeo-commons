2981b9e4a7a03d9aef775e1b9238a22119a9b078
Mathieu Baudier <mbaudier@argeo.org>
2011-08-28 12:52:52 +0000

    Move JCR GIS from Commons to Connect
    git-svn-id: https://svn.argeo.org/commons/trunk@4702 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- server/runtime/org.argeo.server.jcr/src/main/java/org/argeo/jcr/gis/GisJcrConstants.java
+++
@@ -1,8 +0,0 @@
-package org.argeo.jcr.gis;
-
-/** Constants related to GIS in JCR, typically standard paths. */
-public class GisJcrConstants {
-
-	public final static String DATA_STORES_BASE_PATH = "/gis/dataStores";
-
-}
