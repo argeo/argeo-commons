f9cc721289bfc8de1bf4d9ce13d51615dcde9111
Mathieu Baudier <mbaudier@argeo.org>
2011-09-27 20:53:32 +0000

    Keyring login module
    git-svn-id: https://svn.argeo.org/commons/trunk@4761 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ security/plugins/org.argeo.security.equinox/src/main/java/org/argeo/security/equinox/KeyringLoginModule.java
@@ -0,0 +1,62 @@
+package org.argeo.security.equinox;
+
+import java.util.Map;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+
+import org.apache.commons.logging.LogConfigurationException;
+import org.argeo.util.crypto.PasswordBasedEncryption;
+
+public class KeyringLoginModule implements LoginModule {
+	private Subject subject;
+	private CallbackHandler callbackHandler;
+	private PasswordBasedEncryption passwordBasedEncryption;
+
+	public void initialize(Subject subject, CallbackHandler callbackHandler,
+			Map<String, ?> sharedState, Map<String, ?> options) {
+		this.subject = subject;
+		this.callbackHandler = callbackHandler;
+	}
+
+	public boolean login() throws LoginException {
+		Set<PasswordBasedEncryption> pbes = subject
+				.getPrivateCredentials(PasswordBasedEncryption.class);
+		if (pbes.size() > 0)
+			return true;
+		PasswordCallback pc = new PasswordCallback("Master password", false);
+		Callback[] callbacks = { pc };
+		try {
+			callbackHandler.handle(callbacks);
+			passwordBasedEncryption = new PasswordBasedEncryption(
+					pc.getPassword());
+		} catch (Exception e) {
+			throw new LogConfigurationException(e);
+		}
+		return true;
+	}
+
+	public boolean commit() throws LoginException {
+		if (passwordBasedEncryption != null)
+			subject.getPrivateCredentials(PasswordBasedEncryption.class).add(
+					passwordBasedEncryption);
+		return true;
+	}
+
+	public boolean abort() throws LoginException {
+		return true;
+	}
+
+	public boolean logout() throws LoginException {
+		Set<PasswordBasedEncryption> pbes = subject
+				.getPrivateCredentials(PasswordBasedEncryption.class);
+		pbes.clear();
+		return true;
+	}
+
+}
