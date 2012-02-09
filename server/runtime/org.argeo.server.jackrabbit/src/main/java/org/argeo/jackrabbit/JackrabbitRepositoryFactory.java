f4ef63b879433249c89bc031cd03e34864f73bfa
Mathieu Baudier <mbaudier@argeo.org>
2012-02-09 14:09:28 +0000

    Extends Jackrabbit factory capabilities
    git-svn-id: https://svn.argeo.org/commons/trunk@5029 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- server/runtime/org.argeo.server.jackrabbit/src/main/java/org/argeo/jackrabbit/JackrabbitRepositoryFactory.java
+++ server/runtime/org.argeo.server.jackrabbit/src/main/java/org/argeo/jackrabbit/JackrabbitRepositoryFactory.java
@@ -15,54 +15,65 @@ import org.apache.jackrabbit.jcr2dav.Jcr2davRepositoryFactory;
 import org.argeo.ArgeoException;
 import org.argeo.jcr.ArgeoJcrConstants;
 import org.argeo.jcr.DefaultRepositoryFactory;
-import org.osgi.framework.BundleContext;
 
-/** Repository factory which can access remote Jackrabbit repositories */
+/**
+ * Repository factory which can create new repositories and access remote
+ * Jackrabbit repositories
+ */
 public class JackrabbitRepositoryFactory extends DefaultRepositoryFactory
 		implements RepositoryFactory, ArgeoJcrConstants {
 	private final static Log log = LogFactory
 			.getLog(JackrabbitRepositoryFactory.class);
 
-	private BundleContext bundleContext;
-
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Repository getRepository(Map parameters) throws RepositoryException {
+		// check if can be found by alias
 		Repository repository = super.getRepository(parameters);
 		if (repository != null)
 			return repository;
 
-		String uri;
+		// check if remote
+		String uri = null;
 		if (parameters.containsKey(JCR_REPOSITORY_URI))
 			uri = parameters.get(JCR_REPOSITORY_URI).toString();
 		else if (parameters.containsKey(JcrUtils.REPOSITORY_URI))
 			uri = parameters.get(JcrUtils.REPOSITORY_URI).toString();
-		else
-			return null;
 
-		Map<String, String> params = new HashMap<String, String>();
-		params.put(JcrUtils.REPOSITORY_URI, uri);
-		repository = new Jcr2davRepositoryFactory().getRepository(params);
-		if (repository == null)
-			throw new ArgeoException("Remote Davex repository " + uri
-					+ " not found");
-		log.info("Initialized remote Jackrabbit repository from uri " + uri);
+		if (uri == null)
+			repository = createRemoteRepository(uri);
 
-		if (parameters.containsKey(JCR_REPOSITORY_ALIAS)
-				&& bundleContext != null) {
+		if (parameters.containsKey(JCR_REPOSITORY_ALIAS)) {
 			Properties properties = new Properties();
 			properties.putAll(parameters);
-			bundleContext.registerService(Repository.class.getName(),
-					repository, properties);
-			log.info("Registered under alias '"
-					+ parameters.get(JCR_REPOSITORY_ALIAS)
-					+ "' the remote JCR repository from uri " + uri);
+			String alias = parameters.get(JCR_REPOSITORY_ALIAS).toString();
+			publish(alias, repository, properties);
+			log.info("Registered JCR repository under alias '" + alias
+					+ "' with properties " + properties);
 		}
 
 		return repository;
 	}
 
-	public void setBundleContext(BundleContext bundleContext) {
-		this.bundleContext = bundleContext;
+	protected Repository createRemoteRepository(String uri)
+			throws RepositoryException {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put(JcrUtils.REPOSITORY_URI, uri);
+		Repository repository = new Jcr2davRepositoryFactory()
+				.getRepository(params);
+		if (repository == null)
+			throw new ArgeoException("Remote Davex repository " + uri
+					+ " not found");
+		log.info("Initialized remote Jackrabbit repository from uri " + uri);
+		return repository;
+	}
+
+	/**
+	 * Called after the repository has been initialized. Does nothing by
+	 * default.
+	 */
+	@SuppressWarnings("rawtypes")
+	protected void postInitialization(Repository repository, Map parameters) {
+
 	}
 
 }
---
+++ server/runtime/org.argeo.server.jackrabbit/src/main/java/org/argeo/jackrabbit/OsgiJackrabbitRepositoryFactory.java
@@ -0,0 +1,36 @@
+package org.argeo.jackrabbit;
+
+import java.util.Properties;
+
+import javax.jcr.Repository;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.BundleContext;
+
+/**
+ * OSGi-aware Jackrabbit repository factory which can retrieve/publish
+ * {@link Repository} as OSGi services.
+ */
+public class OsgiJackrabbitRepositoryFactory extends
+		JackrabbitRepositoryFactory {
+	private final static Log log = LogFactory
+			.getLog(OsgiJackrabbitRepositoryFactory.class);
+	private BundleContext bundleContext;
+
+	protected void publish(String alias, Repository repository,
+			Properties properties) {
+		if (bundleContext != null) {
+			// do not modify reference
+			Properties props = new Properties(properties);
+			props.setProperty(JCR_REPOSITORY_ALIAS, alias);
+			bundleContext.registerService(Repository.class.getName(),
+					repository, props);
+		}
+	}
+
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+}
