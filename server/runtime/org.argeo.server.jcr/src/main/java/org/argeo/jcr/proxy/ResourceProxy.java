b3c78b01a72d0ddc6e709e6c14ce6e12c2c89270
Mathieu Baudier <mbaudier@argeo.org>
2013-06-02 20:37:32 +0000

    Make proxy more generic https://www.argeo.org/bugzilla/show_bug.cgi?id=162
    git-svn-id: https://svn.argeo.org/commons/branches/1.x@6321 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- server/runtime/org.argeo.server.jcr/src/main/java/org/argeo/jcr/proxy/ResourceProxy.java
+++ server/runtime/org.argeo.server.jcr/src/main/java/org/argeo/jcr/proxy/ResourceProxy.java
@@ -16,19 +16,16 @@
 package org.argeo.jcr.proxy;
 
 import javax.jcr.Node;
-import javax.jcr.Session;
 
 /** A proxy which nows how to resolve and synchronize relative URLs */
 public interface ResourceProxy {
-	/** Path to the proxied node (which may not already exist) */
-	public String getNodePath(String relativePath);
-
 	/**
 	 * Proxy the file referenced by this relative path in the underlying
-	 * repository
+	 * repository. A new session is created by each call, so the underlying
+	 * session of the returned node must be closed by the caller.
 	 * 
-	 * @return the unique identifier of the proxied Node, <code>null</code> if
-	 *         the resource was not found (e.g. HTPP 404)
+	 * @return the proxied Node, <code>null</code> if the resource was not found
+	 *         (e.g. HTTP 404)
 	 */
-	public Node proxy(Session session,String relativePath);
+	public Node proxy(String relativePath);
 }
