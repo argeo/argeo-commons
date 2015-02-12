4b580ca68171ebf18dce390edd5d7502afe7f2ad
Mathieu Baudier <mbaudier@argeo.org>
2015-02-12 19:48:12 +0000

    Introduce ArgeoUser and ArgeoGroup
    git-svn-id: https://svn.argeo.org/commons/trunk@7825 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.security.core/src/org/argeo/security/ArgeoGroup.java
@@ -0,0 +1,7 @@
+package org.argeo.security;
+
+import org.osgi.service.useradmin.Group;
+
+public interface ArgeoGroup extends ArgeoUser, Group, java.security.acl.Group {
+
+}
---
+++ org.argeo.security.core/src/org/argeo/security/ArgeoUser.java
@@ -0,0 +1,9 @@
+package org.argeo.security;
+
+import java.security.Principal;
+
+import org.osgi.service.useradmin.User;
+
+public interface ArgeoUser extends Principal, User {
+
+}
