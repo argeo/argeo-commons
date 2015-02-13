98abc6870c7a7da1fc099fde347f160b92634fd7
Mathieu Baudier <mbaudier@argeo.org>
2015-02-13 23:53:11 +0000

    Move Jackrabbit security model
    git-svn-id: https://svn.argeo.org/commons/trunk@7862 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.security.core/src/org/argeo/security/jcr/jackrabbit/JackrabbitSecurityModel.java
+++ org.argeo.security.core/src/org/argeo/security/jcr/jackrabbit/JackrabbitSecurityModel.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.argeo.security.jackrabbit;
+package org.argeo.security.jcr.jackrabbit;
 
 import java.util.ArrayList;
 import java.util.Iterator;
