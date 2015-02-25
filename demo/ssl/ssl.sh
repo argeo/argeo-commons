1741158d97165aae922222c0d75adc7655485218
Mathieu Baudier <mbaudier@argeo.org>
2015-02-25 17:53:46 +0000

    SSL working
    git-svn-id: https://svn.argeo.org/commons/trunk@7956 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- demo/ssl/ssl.sh
+++ demo/ssl/ssl.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# COMPLETELY UNSAFE - FOR DEVELOPMENT ONLY
+# Run this script from its directory
+
 export OPENSSL_CONF=./openssl.cnf
 export CATOP=./CA
 
@@ -7,30 +10,41 @@ export CATOP=./CA
 
 openssl req -x509 -new -newkey rsa:1024 -extensions server_ext -days 3650 \
  -subj /C=DE/ST=Berlin/O=Example/OU=Systems/CN=localhost/ \
- -keyout server.key -passout pass:demo -out server.crt
+ -keyout newkey.pem -passout pass:demo -out newcrt.pem
  
 openssl pkcs12 -export -passin pass:demo -passout pass:changeit \
- -name "jetty" -inkey server.key -in server.crt \
+ -name "jetty" -inkey newkey.pem -in newcrt.pem \
  -out server.p12
  
  # Convert PKCS12 keystore into a JKS keystore
 keytool -importkeystore \
  -srckeystore server.p12 -srcstoretype pkcs12 -srcstorepass changeit \
  -alias jetty  -destkeystore server.jks -deststorepass changeit
+rm -f server.p12
 
 # Import People CA
 keytool -importcert -keystore server.jks -storepass changeit \
  -alias CA -file CA/cacert.pem
 
+# root user
 openssl req -new -newkey rsa:1024 -extensions server_ext -days 3650 \
  -subj /C=DE/ST=Berlin/O=Example/OU=People/CN=root/ \
- -keyout root.key -passout pass:demo -out root.csr
-openssl ca -batch -passin pass:demo -in root.csr -out root.crt
+ -keyout newkey.pem -passout pass:demo -out newcsr.pem
+openssl ca -batch -passin pass:demo -in newcsr.pem -out newcrt.pem
 openssl pkcs12 -export -passin pass:demo -passout pass:demo \
- -name "root" -inkey root.key -in root.crt \
+ -name "root" -inkey newkey.pem -in newcrt.pem \
  -out root.p12
 
-# Clean
+# demo user
+openssl req -new -newkey rsa:1024 -extensions server_ext -days 3650 \
+ -subj /C=DE/ST=Berlin/O=Example/OU=People/CN=demo/ \
+ -keyout newkey.pem -passout pass:demo -out newcsr.pem
+openssl ca -batch -passin pass:demo -in newcsr.pem -out newcrt.pem
+openssl pkcs12 -export -passin pass:demo -passout pass:demo \
+ -name "demo" -inkey newkey.pem -in newcrt.pem \
+ -out demo.p12
+
+# Clean up
 rm -vf new*.pem
-rm -vf root.csr root.key root.crt
-rm -vf server.p12 server.crt server.key
+#rm -vf root.csr root.key root.crt
+#rm -vf server.p12 server.crt server.key
