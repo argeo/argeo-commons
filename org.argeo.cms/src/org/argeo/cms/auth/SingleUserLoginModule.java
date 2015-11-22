0dfcfef53a629cf38bade4f8605c5b7e507c7436
Mathieu Baudier <mbaudier@argeo.org>
2015-11-22 16:48:03 +0000

    Add single user login module
    git-svn-id: https://svn.argeo.org/commons/trunk@8585 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.cms/src/org/argeo/cms/auth/SingleUserLoginModule.java
@@ -0,0 +1,55 @@
+package org.argeo.cms.auth;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+import javax.security.auth.x500.X500Principal;
+
+import org.apache.jackrabbit.core.security.SecurityConstants;
+import org.apache.jackrabbit.core.security.principal.AdminPrincipal;
+import org.argeo.cms.internal.auth.ImpliedByPrincipal;
+
+public class SingleUserLoginModule implements LoginModule, AuthConstants {
+	private Subject subject;
+
+	@Override
+	public void initialize(Subject subject, CallbackHandler callbackHandler,
+			Map<String, ?> sharedState, Map<String, ?> options) {
+		this.subject = subject;
+	}
+
+	@Override
+	public boolean login() throws LoginException {
+		return true;
+	}
+
+	@Override
+	public boolean commit() throws LoginException {
+		String username = System.getProperty("user.name");
+		X500Principal principal = new X500Principal("uid=" + username
+				+ ",dc=localhost,dc=localdomain");
+		Set<Principal> principals = subject.getPrincipals();
+		principals.add(principal);
+		principals.add(new ImpliedByPrincipal(ROLE_ADMIN, principal));
+		// Jackrabbit
+		principals.add(new AdminPrincipal(SecurityConstants.ADMIN_ID));
+		return true;
+	}
+
+	@Override
+	public boolean abort() throws LoginException {
+		return true;
+	}
+
+	@Override
+	public boolean logout() throws LoginException {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
