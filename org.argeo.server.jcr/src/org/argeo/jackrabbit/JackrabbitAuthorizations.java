f731f7467dfa31933d88a76b6463bc4aea4566b4
Mathieu Baudier <mbaudier@argeo.org>
2016-05-22 11:51:27 +0000

    Deprecate JackrabbitAuthorizations
    git-svn-id: https://svn.argeo.org/commons/trunk@8931 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.server.jcr/src/org/argeo/jackrabbit/JackrabbitAuthorizations.java
+++ org.argeo.server.jcr/src/org/argeo/jackrabbit/JackrabbitAuthorizations.java
@@ -16,7 +16,6 @@
 package org.argeo.jackrabbit;
 
 import java.security.Principal;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.jcr.RepositoryException;
@@ -24,43 +23,42 @@ import javax.jcr.Session;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.jackrabbit.api.JackrabbitSession;
-import org.apache.jackrabbit.api.security.user.Authorizable;
-import org.apache.jackrabbit.api.security.user.UserManager;
-import org.argeo.ArgeoException;
 import org.argeo.jcr.security.JcrAuthorizations;
 
 /** Apply authorizations to a Jackrabbit repository. */
+@Deprecated
 public class JackrabbitAuthorizations extends JcrAuthorizations {
-	private final static Log log = LogFactory
-			.getLog(JackrabbitAuthorizations.class);
+	private final static Log log = LogFactory.getLog(JackrabbitAuthorizations.class);
 
-	private List<String> groupPrefixes = Arrays
-			.asList(new String[] { "ROLE_" });// new ArrayList<String>();
+	// private List<String> groupPrefixes = Arrays.asList(new String[] { "ROLE_"
+	// });// new
+	// ArrayList<String>();
 
 	@Override
-	protected Principal getOrCreatePrincipal(Session session,
-			String principalName) throws RepositoryException {
-		UserManager um = ((JackrabbitSession) session).getUserManager();
-		synchronized (um) {
-			Authorizable authorizable = um.getAuthorizable(principalName);
-			if (authorizable == null) {
-				groupPrefixes: for (String groupPrefix : groupPrefixes) {
-					if (principalName.startsWith(groupPrefix)) {
-						authorizable = um.createGroup(principalName);
-						log.info("Created group " + principalName);
-						break groupPrefixes;
-					}
-				}
-				if (authorizable == null)
-					throw new ArgeoException("Authorizable " + principalName
-							+ " not found");
-			}
-			return authorizable.getPrincipal();
-		}
+	protected Principal getOrCreatePrincipal(Session session, String principalName) throws RepositoryException {
+		String msg = "Use org.argeo.jcr.security.JcrAuthorizations instead of org.argeo.jackrabbit.JackrabbitAuthorizations";
+		log.error(msg);
+		throw new UnsupportedOperationException(msg);
+		// UserManager um = ((JackrabbitSession) session).getUserManager();
+		// synchronized (um) {
+		// Authorizable authorizable = um.getAuthorizable(principalName);
+		// if (authorizable == null) {
+		// groupPrefixes: for (String groupPrefix : groupPrefixes) {
+		// if (principalName.startsWith(groupPrefix)) {
+		// authorizable = um.createGroup(principalName);
+		// log.info("Created group " + principalName);
+		// break groupPrefixes;
+		// }
+		// }
+		// if (authorizable == null)
+		// throw new ArgeoException("Authorizable " + principalName
+		// + " not found");
+		// }
+		// return authorizable.getPrincipal();
+		// }
 	}
 
 	public void setGroupPrefixes(List<String> groupsToCreate) {
-		this.groupPrefixes = groupsToCreate;
+		// this.groupPrefixes = groupsToCreate;
 	}
 }
