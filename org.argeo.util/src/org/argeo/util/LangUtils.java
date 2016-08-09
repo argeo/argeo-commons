82dbb6068c494047bce1ab170f2f6b8b608b2a42
Mathieu Baudier <mbaudier@argeo.org>
2016-08-09 16:02:00 +0000

    Store and load props as properties files.
    git-svn-id: https://svn.argeo.org/commons/trunk@9073 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.util/src/org/argeo/util/LangUtils.java
+++ org.argeo.util/src/org/argeo/util/LangUtils.java
@@ -1,8 +1,15 @@
 package org.argeo.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Map;
+import java.util.Properties;
 
 public class LangUtils {
 	/*
@@ -24,7 +31,7 @@ public class LangUtils {
 	 */
 
 	/**
-	 * Creates a new {@link Dictionary} with one key-value pair (neith key not
+	 * Creates a new {@link Dictionary} with one key-value pair (neither key not
 	 * value should be null)
 	 */
 	public static Dictionary<String, Object> init(String key, Object value) {
@@ -70,6 +77,30 @@ public class LangUtils {
 		}
 		sb.append('}');
 		return sb.toString();
+	}
+
+	public static void storeAsProperties(Dictionary<String, Object> props, Path path) throws IOException {
+		if (props == null)
+			throw new IllegalArgumentException("Props cannot be null");
+		Properties toStore = new Properties();
+		for (Enumeration<String> keys = props.keys(); keys.hasMoreElements();) {
+			String key = keys.nextElement();
+			toStore.setProperty(key, props.get(key).toString());
+		}
+		try (OutputStream out = Files.newOutputStream(path)) {
+			toStore.store(out, null);
+		}
+	}
+
+	public static Dictionary<String, Object> loadFromProperties(Path path) throws IOException {
+		Properties toLoad = new Properties();
+		try (InputStream in = Files.newInputStream(path)) {
+			toLoad.load(in);
+		}
+		Dictionary<String, Object> res = new Hashtable<String, Object>();
+		for (Object key : toLoad.keySet())
+			res.put(key.toString(), toLoad.get(key));
+		return res;
 	}
 
 	/** Singleton constructor. */
