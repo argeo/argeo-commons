bf345b84989cc9a2984bba4390ebdb349701ed8e
mbaudier <mbaudier@mostar>
2017-11-06 16:27:32 +0100

    Improve node RPM packaging

---
+++ dist/argeo-node/rpm/etc/node/settings.sh
@@ -0,0 +1,3 @@
+export LANG=en_US.utf8
+JAVA_OPTS="-showversion -Xmx128m"
+#JAVA_OPTS="-showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
