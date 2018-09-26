57e34e0482e03cea9a6a34326c22c7c969ad07c8
Mathieu Baudier <mbaudier@argeo.org>
2018-09-26 16:06:34 +0200

    Add variables to RCP's Dummy RWT

--- rcp/org.argeo.eclipse.ui.rcp/src/org/eclipse/rap/rwt/client/WebClient.java
+++ rcp/org.argeo.eclipse.ui.rcp/src/org/eclipse/rap/rwt/client/WebClient.java
@@ -5,4 +5,6 @@ public interface WebClient {
 	public final static String PAGE_TITLE = "rcp:PAGE_TITLE";
 	public final static String BODY_HTML = "rcp:BODY_HTML";
 	public final static String THEME_ID = "rcp:THEME_ID";
+	public final static String HEAD_HTML = "rcp:HEAD_HTML";
+	public final static String PAGE_OVERFLOW = "rcp:PAGE_OVERFLOW";
 }
