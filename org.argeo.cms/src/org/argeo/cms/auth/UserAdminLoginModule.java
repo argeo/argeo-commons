4ccae1bf1714f7adbb69a4505f424e70f2c39698
Mathieu Baudier <mbaudier@argeo.org>
2018-11-13 11:02:26 +0100

    Improve certificate auth

--- org.argeo.cms/src/org/argeo/cms/auth/UserAdminLoginModule.java
+++ org.argeo.cms/src/org/argeo/cms/auth/UserAdminLoginModule.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
+import javax.naming.InvalidNameException;
 import javax.naming.ldap.LdapName;
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
@@ -94,8 +95,15 @@ public class UserAdminLoginModule implements LoginModule {
 			// // TODO locale?
 		} else if (sharedState.containsKey(CmsAuthUtils.SHARED_STATE_NAME)
 				&& sharedState.containsKey(CmsAuthUtils.SHARED_STATE_CERTIFICATE_CHAIN)) {
-			// NB: required by Basic http auth
-			username = (String) sharedState.get(CmsAuthUtils.SHARED_STATE_NAME);
+			String certificateName = (String) sharedState.get(CmsAuthUtils.SHARED_STATE_NAME);
+			LdapName ldapName;
+			try {
+				ldapName = new LdapName(certificateName);
+			} catch (InvalidNameException e) {
+				e.printStackTrace();
+				return false;
+			}
+			username = ldapName.getRdn(ldapName.size()-1).getValue().toString();
 			certificateChain = (X509Certificate[]) sharedState.get(CmsAuthUtils.SHARED_STATE_CERTIFICATE_CHAIN);
 			password = null;
 		} else if (singleUser) {
