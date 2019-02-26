796ebdf1479b481f913296622128fb53a3731153
Mathieu Baudier <mbaudier@argeo.org>
2019-02-26 08:55:48 +0100

    Work on GraalVM compatibility

--- org.argeo.cms.e4.rap/cms/app.js
+++ org.argeo.cms.e4.rap/cms/app.js
@@ -1,8 +1,8 @@
-// Standard CMS App
+var AppUi = Java.type('org.argeo.cms.script.AppUi')
+var CmsE4EntryPointFactory = Java.type('org.argeo.cms.e4.rap.CmsE4EntryPointFactory')
+
 APP.webPath = 'cms'
-
-// Common
-APP.pageTitle = 'Argeo CMS';
-
-APP.ui['devops'] = new org.argeo.cms.script.AppUi(APP, new org.argeo.cms.e4.rap.CmsE4EntryPointFactory('org.argeo.cms.e4/e4xmi/cms-devops.e4xmi'));
-APP.ui['devops'].pageTitle = 'Argeo CMS DevOps';
+APP.pageTitle = 'Argeo CMS'
+	
+APP.ui['devops'] = new AppUi(APP, new CmsE4EntryPointFactory('org.argeo.cms.e4/e4xmi/cms-devops.e4xmi'))
+APP.ui['devops'].pageTitle = 'Argeo CMS DevOps'
