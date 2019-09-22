7b4a43f00c979695d4cb9fcd5672b188cb3637a4
Mathieu Baudier <mbaudier@argeo.org>
2019-09-22 09:07:08 +0200

    Make JcrCallback a functional interface.

--- org.argeo.jcr/src/org/argeo/jcr/JcrCallback.java
+++ org.argeo.jcr/src/org/argeo/jcr/JcrCallback.java
@@ -15,9 +15,16 @@
  */
 package org.argeo.jcr;
 
+import java.util.function.Function;
+
 import javax.jcr.Session;
 
 /** An arbitrary execution on a JCR session, optionally returning a result. */
-public interface JcrCallback {
-	public Object execute(Session session);
+@FunctionalInterface
+public interface JcrCallback extends Function<Session, Object> {
+	/** @deprecated Use {@link #apply(Session)} instead. */
+	@Deprecated
+	public default Object execute(Session session) {
+		return apply(session);
+	}
 }
