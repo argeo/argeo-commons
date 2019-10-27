43e8bb2c3186c925991091e2b0f7002a940093c3
Mathieu Baudier <mbaudier@argeo.org>
2019-10-27 12:05:05 +0100

    Add utility to the distinguished name interface.

--- org.argeo.enterprise/src/org/argeo/naming/Distinguished.java
+++ org.argeo.enterprise/src/org/argeo/naming/Distinguished.java
@@ -1,5 +1,9 @@
 package org.argeo.naming;
 
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 import javax.naming.InvalidNameException;
 import javax.naming.ldap.LdapName;
 
@@ -19,5 +23,14 @@ public interface Distinguished {
 		} catch (InvalidNameException e) {
 			throw new IllegalArgumentException("Distinguished name " + dn() + " is not properly formatted.", e);
 		}
+	}
+
+	/** List all DNs of an enumeration as strings. */
+	static Set<String> enumToDns(EnumSet<? extends Distinguished> enumSet) {
+		Set<String> res = new TreeSet<>();
+		for (Enum<? extends Distinguished> enm : enumSet) {
+			res.add(((Distinguished) enm).dn());
+		}
+		return res;
 	}
 }
