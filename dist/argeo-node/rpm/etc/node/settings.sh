8e6b382cfdcef01e4c8dcd13709df0fad0f3b35a
Mathieu Baudier <mbaudier@argeo.org>
2019-12-17 11:16:02 +0100

    Remove reference to 'argeo' user.

--- dist/argeo-node/rpm/etc/node/settings.sh
+++
@@ -1,3 +0,0 @@
-export LANG=en_US.utf8
-JAVA_OPTS="-showversion -Xmx128m"
-#JAVA_OPTS="-showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
