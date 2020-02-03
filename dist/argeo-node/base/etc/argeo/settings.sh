d9b46742e2e1c15f15340af0c05b60a48b416db0
Mathieu Baudier <mbaudier@argeo.org>
2020-02-03 12:15:24 +0100

    Fix remote debugging in settings.sh

--- dist/argeo-node/base/etc/argeo/settings.sh
+++ dist/argeo-node/base/etc/argeo/settings.sh
@@ -5,7 +5,7 @@ JAVA_OPTS="-showversion -Xmx128m"
 #JAVA_OPTS="-showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
 # Development
-#JAVA_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+#JAVA_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,address=*:8000,suspend=n -showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
 # JMX over server tunnel
 #JAVA_OPTS="-showversion -Xmx2048m -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.rmi.port=7084 -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
--- dist/containers/dev-settings.sh
+++ dist/containers/dev-settings.sh
@@ -1,2 +1,2 @@
 export LANG=en_US.utf8
-JAVA_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+JAVA_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,address=*:8000,suspend=n -showversion -Xmx512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7084 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
