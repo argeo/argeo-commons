c08269db6884798c78f39a49dd97bb53740feae6
Mathieu Baudier <mbaudier@argeo.org>
2020-02-19 11:25:32 +0100

    Remove old comments.

--- org.argeo.cms/src/org/argeo/cms/ArgeoTypes.java
+++ org.argeo.cms/src/org/argeo/cms/ArgeoTypes.java
@@ -17,14 +17,7 @@ package org.argeo.cms;
 
 /** JCR types in the http://www.argeo.org/argeo namespace */
 public interface ArgeoTypes {
-//	public final static String ARGEO_LINK = "argeo:link";
-//	public final static String ARGEO_USER_HOME = "argeo:userHome";
-//	public final static String ARGEO_USER_PROFILE = "argeo:userProfile";
 	public final static String ARGEO_REMOTE_REPOSITORY = "argeo:remoteRepository";
-//	public final static String ARGEO_PREFERENCE_NODE = "argeo:preferenceNode";
-
-	// data model
-//	public final static String ARGEO_DATA_MODEL = "argeo:dataModel";
 	
 	// tabular
 	public final static String ARGEO_TABLE = "argeo:table";
