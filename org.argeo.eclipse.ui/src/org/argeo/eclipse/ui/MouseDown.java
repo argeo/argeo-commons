a01f44eb6069d2827a384cc92e29edc724d33295
Mathieu Baudier <mbaudier@argeo.org>
2021-01-20 08:17:35 +0100

    Introduce mouse down functional interface.

---
+++ org.argeo.eclipse.ui/src/org/argeo/eclipse/ui/MouseDown.java
@@ -0,0 +1,26 @@
+package org.argeo.eclipse.ui;
+
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+
+/**
+ * {@link MouseListener#mouseDown(MouseEvent)} as a functional interface in
+ * order to use as a short lambda expression in UI code.
+ * {@link MouseListener#mouseDoubleClick(MouseEvent)} and
+ * {@link MouseListener#mouseUp(MouseEvent)} do nothing by default.
+ */
+@FunctionalInterface
+public interface MouseDown extends MouseListener {
+	@Override
+	void mouseDown(MouseEvent e);
+
+	@Override
+	default void mouseDoubleClick(MouseEvent e) {
+		// does nothing
+	}
+
+	@Override
+	default void mouseUp(MouseEvent e) {
+		// does nothing
+	}
+}
