70a22601db85afd30fb667e7cdf3ba57a5341ad0
Mathieu Baudier <mbaudier@argeo.org>
2022-01-24 05:34:34 +0100

    Experiment with package level A2 metadata

---
+++ org.argeo.api.acr/src/org/argeo/api/acr/uuid/A2.java
@@ -0,0 +1,16 @@
+package org.argeo.api.acr.uuid;
+
+import java.io.Serializable;
+
+/** A2 metadata for this package. */
+class A2 implements Serializable {
+	static final int MAJOR = 2;
+	static final int MINOR = 3;
+
+	static final long serialVersionUID = (long) MAJOR << 32 | MINOR & 0xFFFFFFFFL;
+
+	static {
+//		assert MAJOR == (int) (serialVersionUID >> 32);
+//		assert MINOR == (int) serialVersionUID;
+	}
+}
