e4c639351aa6aa6e2418fdd6f828aa9c4969c4d4
Mathieu Baudier <mbaudier@argeo.org>
2023-06-25 11:46:42 +0200

    Clean up code

--- org.argeo.cms/src/org/argeo/cms/auth/RemoteAuthUtils.java
+++ org.argeo.cms/src/org/argeo/cms/auth/RemoteAuthUtils.java
@@ -46,38 +46,7 @@ public class RemoteAuthUtils {
 	public final static <T> T doAs(Supplier<T> supplier, RemoteAuthRequest req) {
 		CmsSession cmsSession = getCmsSession(req);
 		return CurrentSubject.callAs(cmsSession.getSubject(), () -> supplier.get());
-//		ClassLoader currentContextCl = Thread.currentThread().getContextClassLoader();
-//		Thread.currentThread().setContextClassLoader(RemoteAuthUtils.class.getClassLoader());
-//		try {
-//			return Subject.doAs(
-//					Subject.getSubject((AccessControlContext) req.getAttribute(AccessControlContext.class.getName())),
-//					new PrivilegedAction<T>() {
-//
-//						@Override
-//						public T run() {
-//							return supplier.get();
-//						}
-//
-//					});
-//		} finally {
-//			Thread.currentThread().setContextClassLoader(currentContextCl);
-//		}
 	}
-
-//	public final static void configureRequestSecurity(RemoteAuthRequest req) {
-//		if (req.getAttribute(AccessControlContext.class.getName()) != null)
-//			throw new IllegalStateException("Request already authenticated.");
-//		AccessControlContext acc = AccessController.getContext();
-//		req.setAttribute(REMOTE_USER, CurrentUser.getUsername());
-//		req.setAttribute(AccessControlContext.class.getName(), acc);
-//	}
-//
-//	public final static void clearRequestSecurity(RemoteAuthRequest req) {
-//		if (req.getAttribute(AccessControlContext.class.getName()) == null)
-//			throw new IllegalStateException("Cannot clear non-authenticated request.");
-//		req.setAttribute(REMOTE_USER, null);
-//		req.setAttribute(AccessControlContext.class.getName(), null);
-//	}
 
 	public static CmsSession getCmsSession(RemoteAuthRequest req) {
 		CmsSession cmsSession = (CmsSession) req.getAttribute(CmsSession.class.getName());
