c6b43deb4f238ad6ff797b05f564d824887fd416
Mathieu Baudier <mbaudier@argeo.org>
2025-12-05 08:18:11 +0100

    Introduce Argeo Init as jmod

--- Makefile-init.mk
+++ Makefile-init.mk
@@ -1,4 +1,5 @@
 include sdk.mk
+include sdk/argeo-build/jpms.mk
 
 A2_CATEGORY = org.argeo.cms
 
@@ -15,6 +16,22 @@ clean: osgi-clean
 install: osgi-install
 
 uninstall: osgi-uninstall
+
+#
+# PACKAGING
+#
+
+JMOD_ARGEO_INIT=org.argeo.init
+
+jmod-argeo-init:
+	$(call a2_jmod_prepare_output,$(JMOD_ARGEO_INIT))
+	$(COPY) COPYING.LESSER NOTICE $(JMODS_BASE)/$(JMOD_ARGEO_INIT)/legal
+	
+	# config
+	#mkdir -p $(JMODS_BASE)/$(JMOD_JJML)/man/examples
+	#$(COPY) -v sdk/jbin/*.java $(JMODS_BASE)/$(JMOD_JJML)/man/examples
+
+	$(call a2_jmod_create,$(JMOD_ARGEO_INIT))
 
 include  $(SDK_SRC_BASE)/sdk/argeo-build/osgi.mk
 
